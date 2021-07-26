68f603dfa6fdea7e20cd0b57b8a970d9851502bf
gtang1626 <gtang1626@gmail.com>
2021-07-26 17:03:37 +0000

    click listener for sign in button

---
+++ public/js/signIn.js
@@ -0,0 +1,8 @@
+console.log("signIn loaded");
+
+const signInButton = document.querySelector(".button");
+console.log(signInButton);
+
+signInButton.addEventListener("click", () => {
+    console.log("clicked");
+})
